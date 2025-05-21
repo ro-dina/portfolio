95b9708543d2234cc4b96fe4077568820fd2f7f3
ro-dina <131938022+ro-dina@users.noreply.github.com>
2025-05-22 08:38:17 +0900

    スキルの追記

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
+//モーダル
+let currentIndex = 0;
+let modalImages = [];
+
+function openModal(imgElement) {
+    const group = imgElement.getAttribute('data-group');
+    modalImages = Array.from(document.querySelectorAll(`img[data-group='${group}']`));
+    currentIndex = modalImages.indexOf(imgElement);
+
+    const modal = document.getElementById("imageModal");
+    const modalImg = document.getElementById("modalImage");
+    modal.style.display = "flex";
+    modalImg.src = imgElement.src;
+}
+
+function closeModal() {
+    document.getElementById("imageModal").style.display = "none";
+}
+
+function changeModalSlide(direction) {
+    if (!modalImages.length) return;
+
+    currentIndex = (currentIndex + direction + modalImages.length) % modalImages.length;
+    const newImg = modalImages[currentIndex];
+    const modalImg = document.getElementById("modalImage");
+    modalImg.src = newImg.src;
+}
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
     const sliders = document.querySelectorAll('.slider');
     const slideIndexes = new Array(sliders.length).fill(0);
@@ -26,5 +56,11 @@ document.addEventListener('DOMContentLoaded', () => {
         setInterval(() => {
             changeSlide(1);
         }, 5000);
+
+        slides.forEach((img) => {
+            img.addEventListener('click', () => {
+                openModal(img);
+            });
+        });
     });
 });
